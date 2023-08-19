305f8ed1245300940929962cf1f1f2bd24184fa1
SOMNATH <somnathnikam2076@gmail.com>
2023-08-19 19:06:54 +0530

    Commit 1.0

--- app/src/main/java/com/example/farmhelp1/LoginActivity.kt
+++ app/src/main/java/com/example/farmhelp1/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.example.farmhelp1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val register = findViewById<Button>(R.id.register)
+        val login = findViewById<Button>(R.id.login)
+
+        register.setOnClickListener {
+            val i = Intent(applicationContext,RegisterActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+        login.setOnClickListener {
+            val i = Intent(applicationContext,RegisterActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
