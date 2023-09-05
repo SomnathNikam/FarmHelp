4327c7e4fae4e1e7837f5bae8e0993d33aa2ac77
SOMNATH <somnathnikam2076@gmail.com>
2023-09-05 16:27:02 +0530

    Second Commit of the day

--- app/src/main/java/com/example/farmhelp1/MainActivity.kt
+++ app/src/main/java/com/example/farmhelp1/MainActivity.kt
@@ -2,22 +2,41 @@ package com.example.farmhelp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import com.example.farmhelp1.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        replaceFragment(Home())
 
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.info -> replaceFragment(info())
+                R.id.schemes -> replaceFragment(schemes())
+                R.id.about_us -> replaceFragment(About())
+                else -> {
 
+                }
+            }
+            true
+        }
 
     }
+    private fun replaceFragment(fragment: Fragment){
 
+        val fragmentManager = supportFragmentManager
+        val FragmentTransaction = fragmentManager.beginTransaction()
+        FragmentTransaction.replace(R.id.frmelayout,fragment)
+    }
 
 
 
