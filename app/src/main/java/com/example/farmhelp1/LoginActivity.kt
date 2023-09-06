ebbf9a97eb269771fcd3895adf9e2608d90c0589
SOMNATH <somnathnikam2076@gmail.com>
2023-09-07 04:27:34 +0530

    Works 1.0

--- app/src/main/java/com/example/farmhelp1/LoginActivity.kt
+++ app/src/main/java/com/example/farmhelp1/LoginActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.farmhelp1.databinding.ActivityLoginBinding
+import com.example.farmhelp1.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
@@ -12,14 +16,39 @@ import com.google.firebase.ktx.Firebase
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var binding:ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // ...
 // Initialize Firebase Auth
-        //auth = Firebase.auth
+
+        auth = Firebase.auth
+        binding.btnSignup.setOnClickListener{
+            auth.signInWithEmailAndPassword(binding.Email.text.toString(), binding.textpassword.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                   //     Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        Toast.makeText(this,task.result.toString(),Toast.LENGTH_LONG).show()
+
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                      //  Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                    }
+                }
+
+        }
         
         
         
--- app/src/main/java/com/example/farmhelp1/SplashAnimActivity2.kt
+++ app/src/main/java/com/example/farmhelp1/SplashAnimActivity2.kt
@@ -12,7 +12,7 @@ class SplashAnimActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_anim2)
         val lottieAnimationView = findViewById<LottieAnimationView>(R.id.splashanim)
-        Handler(Looper.getMainLooper()).postDelayed(Runnable {  startActivity(Intent(this,MainActivity::class.java))
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {  startActivity(Intent(this,LoginActivity::class.java))
             finish() },4000)
 
     }
