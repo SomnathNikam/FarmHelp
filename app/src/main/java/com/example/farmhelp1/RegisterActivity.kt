f50744abb91a594750b9bfd231b2ba689daee549
SOMNATH <somnathnikam2076@gmail.com>
2023-09-08 04:49:59 +0530

    Phone No Auth Working

--- app/src/main/java/com/example/farmhelp1/RegisterActivity.kt
+++ app/src/main/java/com/example/farmhelp1/RegisterActivity.kt
@@ -4,15 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.farmhelp1.databinding.ActivityMainBinding
+import com.example.farmhelp1.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
+
+    private  lateinit var binding:ActivityRegisterBinding
+    private  lateinit var auth:FirebaseAuth
+    private var currentUser:FirebaseUser ?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-        val btn = findViewById<Button>(R.id.button)
-        btn.setOnClickListener{
-            val i = Intent(this,MainActivity::class.java)
-            startActivity(i)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+        currentUser = auth.currentUser
+
+    }
+    fun logout(){
+        auth.signOut()
+        login()
+    }
+    fun login(){
+        val login = Intent(this@RegisterActivity,PhoneAuth::class.java)
+        startActivity(login)
+        finish()
+    }
+    override  fun onStart(){
+        super.onStart()
+        if(currentUser==null){
+            login()
+        }
+        else
+        {
+            var userInfo = currentUser!!.phoneNumber
+            userInfo.toString()
+
+
         }
     }
 }
