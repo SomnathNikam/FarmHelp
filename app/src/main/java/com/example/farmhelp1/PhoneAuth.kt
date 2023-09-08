7c2a2d1b0f3544fbe2dcd17005b01093e487b38e
SOMNATH <somnathnikam2076@gmail.com>
2023-09-08 05:34:44 +0530

    working is continued

--- app/src/main/java/com/example/farmhelp1/PhoneAuth.kt
+++ app/src/main/java/com/example/farmhelp1/PhoneAuth.kt
@@ -3,9 +3,20 @@ package com.example.farmhelp1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.farmhelp1.databinding.ActivityPhoneAuthBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.PhoneAuthProvider.*
+import java.util.concurrent.TimeUnit
 
 class PhoneAuth : AppCompatActivity() {
     private lateinit var binding: ActivityPhoneAuthBinding
@@ -25,19 +36,117 @@ class PhoneAuth : AppCompatActivity() {
         startActivity(login)
         finish()
     }
-    override  fun onStart(){
+    override  fun onStart() {
         super.onStart()
-        if(currentUser==null){
+        if (currentUser == null) {
             send_home()
             finish()
-        }
-        else
-        {
+        } else {
             var userInfo = currentUser!!.phoneNumber
             userInfo.toString()
 
 
+        }
+    }
 
+    fun generateOtp(view: View) {
+
+        phoneNumber = binding.button2.text.toString()
+        if (phoneNumber!=null){
+
+            val options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber) // Phone number to verify
+                .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                .setActivity(this) // Activity (for callback binding)
+                .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+                .build()
+
+            verifyPhoneNumber(options)
+        }
+    }
+    private val callbacks = object:PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+    //        Log.d(TAG, "onVerificationCompleted:$credential")
+            signInWithPhoneAuthCredential(credential)
         }
 
-}
+        private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener(this@PhoneAuth){ task ->
+
+                //.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        // Log.d(TAG, "signInWithCredential:success")
+
+                        val user = task.result?.user
+                    } else {
+                        // Sign in failed, display a message and update the UI
+                        // Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            // The verification code entered was invalid
+                        }
+                        // Update UI
+                    }
+                }
+        }
+
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+          //  Log.w(TAG, "onVerificationFailed", e)
+
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+            } else if (e is FirebaseAuthMissingActivityForRecaptchaException) {
+                // reCAPTCHA verification attempted with null Activity
+            }
+
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: ForceResendingToken,
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            //Log.d(TAG, "onCodeSent:$verificationId")
+
+            // Save verification ID and resending token so we can use them later
+            //storedVerificationId = verificationId
+            //resendToken = token
+        }
+    }
+//    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+//        auth.signInWithCredential(credential)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                   // Log.d(TAG, "signInWithCredential:success")
+//
+//                    val user = task.result?.user
+//                } else {
+//                    // Sign in failed, display a message and update the UI
+//                   // Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+//                        // The verification code entered was invalid
+//                    }
+//                    // Update UI
+//                }
+
+    }
+
+
