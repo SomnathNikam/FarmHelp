37dd1ecc5b3434a2b7b1e25d473b3efebb0576f5
SOMNATH <somnathnikam2076@gmail.com>
2023-09-09 04:32:29 +0530

    commit

--- app/src/main/java/com/example/farmhelp1/info.kt
+++ app/src/main/java/com/example/farmhelp1/info.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.farmhelp1.databinding.ActivityOtpBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,8 +22,10 @@ class info : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -55,5 +58,7 @@ class info : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
     }
+
 }
