72a422a48c3f0fcca85480dedcc9d984431d20e9
SOMNATH <somnathnikam2076@gmail.com>
2023-09-19 01:32:35 +0530

    specific

--- app/src/main/java/com/example/farmhelp1/MainActivity.kt
+++ app/src/main/java/com/example/farmhelp1/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       replaceFragment(Home().requireParentFragment())
+        
 
 
 
