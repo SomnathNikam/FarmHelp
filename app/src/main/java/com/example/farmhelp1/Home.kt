acf5844fb5ed2329084f55a5874bc7a8c61ec13f
SOMNATH <somnathnikam2076@gmail.com>
2023-09-19 02:17:23 +0530

    modified Home.kt

--- app/src/main/java/com/example/farmhelp1/Home.kt
+++ app/src/main/java/com/example/farmhelp1/Home.kt
@@ -1,10 +1,14 @@
 package com.example.farmhelp1
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import com.example.farmhelp1.databinding.FragmentInfoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,13 +17,17 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragm.newInstance] factory method to
+ * Use the [Home.newInstance] factory method to
  * create an instance of this fragment.
  */
 class Home : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentInfoBinding? =null
+    private val binding get() = _binding!!
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +42,43 @@ class Home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        _binding = FragmentInfoBinding.inflate(inflater,container,false)
+        val list = arrayOf("Abhay","Joseph","Haria","Avni","Apoorva","Chris","Dravid","Kaira","Dwayne","Christoper",
+            "Jim","Russel","Donald","Brack","Vladimir")
+
+        val listAdapter: ArrayAdapter<*>
+
+        listAdapter= ArrayAdapter(requireContext(),
+            R.layout.simple_list_item_1,
+            list)
+        binding.listview1.adapter=listAdapter
+
+        binding.searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                binding.searchBar.clearFocus()
+                if (list.contains(p0)) {
+                    listAdapter.filter.filter(p0)
+                }
+                return false
+            }
+
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+
+                listAdapter.filter.filter(p0)
+                return false
+            }
+        })
+
+
+
+
+        return binding.root
     }
+
 
     companion object {
         /**
