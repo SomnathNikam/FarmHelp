0309d52e1a6247ee53b89342479dd2a7d7962321
SOMNATH <somnathnikam2076@gmail.com>
2023-09-28 01:49:06 +0530

    working

--- app/src/main/java/com/example/farmhelp1/SplashActivity.kt
+++ app/src/main/java/com/example/farmhelp1/SplashActivity.kt
@@ -6,22 +6,23 @@ import android.os.Handler
 import android.os.Looper
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.example.farmhelp1.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
-    private  lateinit var  binding: SplashActivity()
+    private  lateinit var  binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=SplashActivity().layoutInflater.inflate(this)
-        setContentView(R.layout.activity_splash)
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
 
             val Animation = AnimationUtils.loadAnimation(this,R.anim.fade_in)
-
+            binding.farmlogo.startAnimation(Animation)
 
             startActivity(Intent(this,MainActivity::class.java))
             finish()
-        },3000)
+        },4000)
 
     }
 }
