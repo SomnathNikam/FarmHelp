196e2e4ce463f4b5c5ba9e9490cbe4b8d34f0e63
SOMNATH <somnathnikam2076@gmail.com>
2023-09-30 00:48:39 +0530

    1.0

--- app/src/main/java/com/example/farmhelp1/MainActivity.kt
+++ app/src/main/java/com/example/farmhelp1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.farmhelp1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.farmhelp1.Fragments.Home
@@ -10,6 +12,7 @@ import com.example.farmhelp1.Fragments.schemes
 import com.example.farmhelp1.R.id.frmelayout
 import com.example.farmhelp1.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
 
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -24,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         var  bottombar = findViewById<BottomNavigationView>(R.id.bottom_nav)
 
-
+        val floatingbtn = findViewById<FloatingActionButton>(R.id.floatingbtn)
 
         replaceFragment(Home())
 
@@ -44,6 +49,10 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        floatingbtn.setOnClickListener{
+
+        }
+
     }
     private fun replaceFragment(fragment: Fragment){
 
@@ -51,6 +60,11 @@ class MainActivity : AppCompatActivity() {
         val FragmentTransaction = fragmentManager.beginTransaction()
         FragmentTransaction.replace(frmelayout,fragment)
         FragmentTransaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu,menu)
+        return true
     }
 
 
