c9991edb2884164de4c560039c2069005f38822a
SOMNATH <somnathnikam2076@gmail.com>
2023-10-01 11:54:57 +0530

    1.0

--- app/src/main/java/com/example/farmhelp1/chatbotActivity2.kt
+++ app/src/main/java/com/example/farmhelp1/chatbotActivity2.kt
@@ -1,11 +1,18 @@
 package com.example.farmhelp1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class chatbotActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatbot2)
+
+        val backarrow = findViewById<ImageView>(R.id.img_back_arrow)
+        backarrow.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
